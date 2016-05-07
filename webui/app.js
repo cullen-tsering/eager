30f9ede609146497ef99d8954e64de3ed26a0258
cullen-tsering <cullen.tsering@live.com>
2016-05-06 19:35:44 -0500

    say hi

--- webui/app.js
+++ webui/app.js
@@ -1,13 +1,31 @@
-var express =require("express");
-var moment = require("moment");
-var solid = require("consolidate");
-var app = express();
+var express =require("express"),
+ moment = require("moment"),
+ solid = require("consolidate"),
+    jade = require("jade"),
+ app = express();
+
+app.engine('html', solid.jade);
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
     res.send('hello, cullen on ' + moment().format());
-    
 });
 
+app.get('/hi', function (req, res, next) {
+    //res.send('hello, cullen on ' + moment().format());
+    console.log("hi");
+    var data = {
+        pageTitle:'hi',
+        foo:true,
+        bar: function(a){
+            console.log(a);
+        },
+        youAreUsingJade: true
+    };
+    res.render('hi', data);
+});
 app.listen(process.env.PORT || 3000, function(){
     console.log("app started: " + moment().format());
 });

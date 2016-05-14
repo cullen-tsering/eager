b8ca71c09931514b9182c202311f3bf7c9df9f67
cullen-tsering <cullen.tsering@live.com>
2016-05-14 17:50:37 -0500

    using log4node

--- webui/app.js
+++ webui/app.js
@@ -1,8 +1,13 @@
 var express =require("express"),
- moment = require("moment"),
- solid = require("consolidate"),
-    jade = require("jade"),
- app = express();
+    moment = require("moment"),
+    solid = require("consolidate"),
+    jade = require("jade");
+    
+var log4node = require("../modules/log4node/log4node.js");
+var logger = new log4node("log.log");
+var app = express();
+ 
+ 
 
 app.engine('html', solid.jade);
 
@@ -10,7 +15,9 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
-    res.send('hello, cullen on ' + moment().format());
+    var msg = 'hello, cullen on ' + moment().format();
+    res.send(msg);
+    logger.log_debug(msg)
 });
 
 app.get('/hi', function (req, res, next) {
@@ -26,6 +33,13 @@ app.get('/hi', function (req, res, next) {
     };
     res.render('hi', data);
 });
+
+app.get('/upload', function(req, res, next){
+    res.render('upload');
+});
+app.post('/upload', function(req, res, next){
+    console.log(req);
+});
 app.listen(process.env.PORT || 3000, function(){
-    console.log("app started: " + moment().format());
+    console.log("app started: " + moment().format() + " on port: " + (process.env.PORT || 3000));
 });
